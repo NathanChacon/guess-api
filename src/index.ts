8d796d62cbddf186ae73dce22b356fc657b438c2
Nathan Chacon <chacondorodo@gmail.com>
2024-04-04 20:05:03 -0300

    Initial project setup

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors'
+import { Server, Socket } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+      }
+});
+app.use(cors());
+
+io.on('connection', (socket: Socket) => {
+    console.log('A user connected');
+
+
+    socket.on('chat message', (msg: string) => {
+        console.log(msg)
+        console.log('message: ' + msg);
+
+
+        io.emit('chat message', msg);
+    });
+
+    // Handle 'disconnect' event
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
